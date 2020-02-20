3d19254deb3a6257ac3476e583171d8e07b6c51a
przemyslawgaldys <przemyslaw.galdys@dcx.bz>
2020-02-20 20:44:34 +0100

    refactor: prepear project to external changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,25 +8,14 @@ module.exports = function(grunt) {
             },
             dist: {
                 files: {
-                    'style.css': 'style.sass'
+                    'src/style.css': 'src/style.sass'
                 }
-            }
-        },
-
-        imagemin: {
-            dynamic: {
-                files: [{
-                    expand: true,
-                    cwd: 'images/',
-                    src: ['**/*.{png,jpg,gif}'],
-                    dest: 'images/build/'
-                }]
             }
         },
 
         watch: {
             scripts: {
-                files: ['style.sass'],
+                files: ['src/style.sass'],
                 tasks: ['sass'],
                 options: {
                     spawn: false,
@@ -36,7 +25,7 @@ module.exports = function(grunt) {
 
         browserSync: {
             bsFiles: {
-                src: 'style.css'
+                src: 'src/style.css'
             },
             options: {
                 watchTask: true,
@@ -50,10 +39,9 @@ module.exports = function(grunt) {
 
     // Load the plugins tasks
     grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-browser-sync');
 
     // Default task(s).
-    grunt.registerTask('default', ["sass", "imagemin", "browserSync", "watch"]);
+    grunt.registerTask('default', ["sass", "browserSync", "watch"]);
 };
